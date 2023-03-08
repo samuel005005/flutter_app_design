b2c3871098e7fd79955e5ce675698c911d93bc05
Samuel Paez <spaez@viva.com.do>
2023-03-08 00:25:17 -0400

    prosgress

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CircularProgressPage extends StatefulWidget {
@@ -7,17 +11,95 @@ class CircularProgressPage extends StatefulWidget {
   State<CircularProgressPage> createState() => _CircularProgressPageState();
 }
 
-class _CircularProgressPageState extends State<CircularProgressPage> {
+class _CircularProgressPageState extends State<CircularProgressPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+
+  double percentage = 0.0;
+  double newPercentage = 0.0;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 800));
+    animationController.addListener(() {
+      if (kDebugMode) {
+        print('Valor Controller : ${animationController.value}');
+      }
+
+      setState(() {
+        percentage =
+            lerpDouble(percentage, newPercentage, animationController.value)!;
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          percentage = newPercentage;
+          newPercentage += 10;
+          if (newPercentage > 100) {
+            newPercentage = 0;
+          }
+          animationController.forward(from: 0.0);
+          setState(() {});
+        },
+        backgroundColor: Colors.pink,
+        child: const Icon(Icons.refresh),
+      ),
       body: Center(
         child: Container(
+          padding: const EdgeInsets.all(5),
           width: 300,
           height: 300,
-          color: Colors.red,
+          child: CustomPaint(
+            painter: _MyRadialProsgress(percentage: percentage),
+          ),
         ),
       ),
     );
   }
+}
+
+class _MyRadialProsgress extends CustomPainter {
+  final double percentage;
+  _MyRadialProsgress({required this.percentage});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Circle Completed
+    final paint = Paint()
+      ..strokeWidth = 4
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke;
+
+    final Offset center = Offset(size.width * .5, size.height * .5);
+    final double radius = min(size.width * .5, size.height * .5);
+
+    canvas.drawCircle(center, radius, paint);
+    // Arco
+    final painArco = Paint()
+      ..strokeWidth = 10
+      ..color = Colors.pink
+      ..style = PaintingStyle.stroke;
+
+    double arcAngle = 2 * pi * (percentage / 100);
+
+    canvas.drawArc(Rect.fromCircle(center: center, radius: radius),
+        (3 * pi) / 2, arcAngle, false, painArco);
+  }
+
+  @override
+  bool shouldRepaint(oldDelegate) => true;
 }
