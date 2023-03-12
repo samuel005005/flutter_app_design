972d2ede02ee730ba35eb77ab749ee919b162582
Samuel Paez <spaez@viva.com.do>
2023-03-12 10:56:58 -0400

    terminando SlideShow

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -9,20 +9,34 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SlideShow(
-        indicationUpPosition: false,
-        primaryColor: Colors.deepPurple,
-        secundaryColor: Colors.red,
-        primaryBullet: 15,
-        secundaryBullet: 12,
-        slides: <Widget>[
-          SvgPicture.asset('assets/svgs/slide-1.svg'),
-          SvgPicture.asset('assets/svgs/slide-2.svg'),
-          SvgPicture.asset('assets/svgs/slide-3.svg'),
-          SvgPicture.asset('assets/svgs/slide-4.svg'),
-          SvgPicture.asset('assets/svgs/slide-5.svg'),
+      body: Column(
+        children: const [
+          Expanded(child: MySlideShow()),
+          Expanded(child: MySlideShow())
         ],
       ),
+    );
+  }
+}
+
+class MySlideShow extends StatelessWidget {
+  const MySlideShow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideShow(
+      indicationUpPosition: false,
+      primaryColor: Colors.deepPurple,
+      secundaryColor: Colors.red,
+      primaryBullet: 15,
+      secundaryBullet: 12,
+      slides: <Widget>[
+        SvgPicture.asset('assets/svgs/slide-1.svg'),
+        SvgPicture.asset('assets/svgs/slide-2.svg'),
+        SvgPicture.asset('assets/svgs/slide-3.svg'),
+        SvgPicture.asset('assets/svgs/slide-4.svg'),
+        SvgPicture.asset('assets/svgs/slide-5.svg'),
+      ],
     );
   }
 }
