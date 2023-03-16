120c98896885d53a97b76382290c60f8530f2b4b
Samuel Paez Perez <spaez@viva.com.do>
2023-03-16 16:02:14 -0400

    sliver

--- lib/src/pages/sliver_list_page.dart
+++ lib/src/pages/sliver_list_page.dart
@@ -5,28 +5,130 @@ class SliverListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _MainScroll(),
+    return Scaffold(
+      body: Stack(
+        children: [
+          _MainScroll(),
+          const Positioned(
+            bottom: -10,
+            right: 0,
+            child: _ButtonNewList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ButtonNewList extends StatelessWidget {
+  const _ButtonNewList();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width * .9,
+      height: 100,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50),
+            ),
+          ),
+          backgroundColor: const Color(
+            0xffED6762,
+          ),
+        ),
+        child: const Text(
+          "CREATE NEW LIST",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 3,
+          ),
+        ),
+      ),
     );
   }
 }
 
 class _MainScroll extends StatelessWidget {
-  const _MainScroll();
+  final items = [
+    const _ItemSilver('Orange', Color(0xffF08F66)),
+    const _ItemSilver('Family', Color(0xffF2A38A)),
+    const _ItemSilver('Subscriptions', Color(0xffF7CDD5)),
+    const _ItemSilver('Books', Color(0xffFCEBAF)),
+    const _ItemSilver('Orange', Color(0xffF08F66)),
+    const _ItemSilver('Family', Color(0xffF2A38A)),
+    const _ItemSilver('Subscriptions', Color(0xffF7CDD5)),
+    const _ItemSilver('Books', Color(0xffFCEBAF)),
+  ];
+
+  _MainScroll();
 
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       physics: const BouncingScrollPhysics(),
       slivers: [
-        const SliverAppBar(
-          backgroundColor: Colors.red,
+        // const SliverAppBar(
+        //   floating: true,
+        //   elevation: 0,
+        //   centerTitle: true,
+        //   backgroundColor: Colors.red,
+        //   title: Text("Hello Word"),
+        // ),
+
+        SliverPersistentHeader(
+          floating: true,
+          delegate: _SliverCustomHeaderDelegate(
+            maxHeight: 220,
+            minHeight: 200,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              color: Colors.white,
+              child: const _Title(),
+            ),
+          ),
         ),
         SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) => null),
-        )
+          delegate:
+              SliverChildListDelegate([...items, const SizedBox(height: 100)]),
+        ),
       ],
     );
+  }
+}
+
+class _SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  _SliverCustomHeaderDelegate(
+      {required this.minHeight, required this.maxHeight, required this.child})
+      : assert(maxHeight > minHeight);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  bool shouldRebuild(covariant _SliverCustomHeaderDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
   }
 }
 
